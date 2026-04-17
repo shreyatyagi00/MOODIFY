fce4715b149f0272e623fa2ed8d972c2a444a118
Shreya Tyagi <shreyaatyagii@gmail.com>
2026-04-18 00:34:25 +0530

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,14 @@ dns.setDefaultResultOrder("ipv4first");
 require("dotenv").config();
 const app = require("./src/app")
 const connectToDB = require("./src/config/database");
+const PORT = process.env.PORT || 3000;
 
-
-connectToDB();
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+connectToDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error("DB connection failed", err);
+  });
