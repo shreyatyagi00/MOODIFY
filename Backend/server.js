66dfbac75c3f5db33dc5a6405b6b2e57c41d01c9
Shreya Tyagi <shreyaatyagii@gmail.com>
2026-04-18 00:39:03 +0530

    fix: async server start

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,23 @@
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 dns.setDefaultResultOrder("ipv4first");
+
 require("dotenv").config();
-const app = require("./src/app")
+
+const app = require("./src/app");
 const connectToDB = require("./src/config/database");
+
 const PORT = process.env.PORT || 3000;
 
-connectToDB()
-  .then(() => {
+const startServer = async () => {
+  try {
+    await connectToDB(); // 👈 yaha await use hoga
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-  })
-  .catch(err => {
+  } catch (err) {
     console.error("DB connection failed", err);
-  });
+  }
+};
+
+startServer();
